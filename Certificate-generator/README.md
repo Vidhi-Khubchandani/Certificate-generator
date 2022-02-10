f433549926467ee5e0c01b019c5d5bd8341c4525
Vidhi-Khubchandani <77888989+Vidhi-Khubchandani@users.noreply.github.com>
2022-02-10 18:13:10 +0530

    Add files via upload

---
+++ Certificate-generator/README.md
@@ -0,0 +1,7 @@
+# Certificate-generator
+This project is used to generate certificates with a list of names given as input
+
+## Insight
+This python project uses open cv for the automatic generation of the certificates and you can choose the template of your choice and it just needs a list of names to generate the certificate for.
+
+It is useful for creating certificates for a large number of attendees with minimum possible error.
---
+++ Certificate-generator/main.py
@@ -0,0 +1,34 @@
+import os
+import cv2
+
+list_of_names = []
+
+def delete_old_data():
+    for certificates in os.listdir("generated-certificates/"):
+        os.remove("generated-certificates/{}".format(certificates))
+
+
+def cleanup_data():
+    with open("name-data-file.txt") as file:
+        for line in file:
+            list_of_names.append(line.strip())
+
+def generate_certificates():
+
+    for index,name in enumerate(list_of_names):
+        certificate_template_image = cv2.imread("certificate-template.jpg")
+        cv2.putText(certificate_template_image, name.strip(), (900,1500), cv2.FONT_HERSHEY_SIMPLEX, 5, (0,0,250), 5, cv2.LINE_AA)
+        cv2.imwrite("generated-certificates/{}.jpg".format(name.strip()),certificate_template_image)
+        print("Processing{}/{}".format(index+1,len(list_of_names)))
+
+def main():
+    delete_old_data()
+    cleanup_data()
+    generate_certificates()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
